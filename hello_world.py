30ff53c168e96485a87ee28ef0ce89a7ae419ec0
lottieswift <lottie.swift93@gmail.com>
2017-02-15 21:52:22 +0000

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,66 @@
+print 'Hello world'
+
+print 1+2
+#3
+print 5-6
+#-1
+print 8*9
+#72
+print 6 / 2
+#3
+print 5 / 2
+#2
+print 5.0 / 2
+#2.5
+print 5 % 2
+#1
+print "hello" + "world"
+#hello world
+print "Bob" * 3
+#BobBobBob
+
+print "hello".upper()
+#HELLO
+print "GOODBYE".lower()
+#goodbye
+print "the lord of the rings".title()
+#The Lord Of The Rings
+
+age = 5
+like = "painting"
+age_description = "my age is {age} and I like {like}.format(age = age, like = like)"
+
+a = 1
+a+1
+print a
+#1
+a = a + 1
+print a
+#2
+b = "hello"
+print b
+#hello
+c = b.title()
+print b
+#hello
+
+print c
+#Hello
+
+d = "hello"
+
+e = d.title()
+print d
+#hello
+print e
+#Hello
+
+name = "Dave"
+f = "Hello {0}! ".format(name)
+print f
+#Hello Dave!
+name = "Sarah"
+print f
+#Hello Dave!
+print f * 5
+#Hello Dave!Hello Dave!Hello Dave!Hello Dave!Hello Dave!
